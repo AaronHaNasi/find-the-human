49abd5df148f4a2c70dd2efd5f2041fcf1373dd8
Aaron Bager <aaron@michael.medialinkwifi.com>
2020-11-29 16:50:10 -0500

    Added bot query
    Player1 will be queried for how many bots should be generated for the
    game

--- Game.java
+++ Game.java
@@ -4,7 +4,15 @@ import java.net.*;
 public class Game extends Thread{
 	private Socket[] players; 
 
-	public Game(Socket[] players) {
-		this.players = players; 
+	public Game(Socket[] players) {	this.players = players; }
+
+	public void run() {
+		DataOutputStream toPlayer1 = new DataOutputStream(players[0].getOutputStream());
+		BufferedReader fromPlayer1 = new BufferedReader(new InputStreamReader(players[0].getInputStream()));
+		int numberOfBots = Integer.parseInt(fromPlayer1.readLine());
+
+		
+
+		// int botNumber = fromPlayer1.read(4);
 	}
 }
